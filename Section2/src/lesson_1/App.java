5631c5d50ac33e9769e659cc6b30d98938a2dad7
Fazlur Abba <fazlur.abba@gmail.com>
2020-12-02 23:26:30 +0000

    Lesson 11 Objects, Messages and Dependencies

---
+++ Section2/src/lesson_1/App.java
@@ -0,0 +1,11 @@
+package lesson_1;
+
+public class App {
+    public static void main(String[] args) {
+        Vehicle myVehicle = new Vehicle();
+        Vehicle anotherVehicle = new Vehicle();
+
+        myVehicle.accelerate();
+        anotherVehicle.accelerate();
+    }
+}
---
+++ Section2/src/lesson_1/Vehicle.java
@@ -0,0 +1,13 @@
+package lesson_1;
+
+public class Vehicle {
+    public void startEngine(){
+        System.out.println("Starting Engine...");
+    }
+    public void stopEngine(){
+        System.out.println("Stop Engine...");
+    }
+    public void accelerate(){
+        System.out.println("Accelerating myself...");
+    }
+}
